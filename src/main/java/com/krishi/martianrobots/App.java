4810bb01486ef70fc665f4528209d5587b572622
Krishi Shah <krishikshah@gmail.com>
2017-11-13 17:49:25 +0000

    readme update

--- src/main/java/com/krishi/martianrobots/App.java
+++ src/main/java/com/krishi/martianrobots/App.java
@@ -3,10 +3,6 @@ package com.krishi.martianrobots;
 public class App {
 
     public static void main(String[] args) {
-        //Pre-condition
-        //Assert only one argument has been provided which specifies a file directory
-        //Assert input coordinates <= 50
-        //Assert instruction strings length <= 100 chars
         String fileDir = args[0];
 
         InputParser parser = new InputParser(fileDir);
